a485446f6198c0f4212a72b4254aba7ede047fb6
wobsoriano <sorianorobertc@gmail.com>
2025-03-03 09:42:44 -0800

    add autorhized party

--- packages/laravel-clerk/src/Guards/ClerkGuard.php
+++ packages/laravel-clerk/src/Guards/ClerkGuard.php
@@ -87,7 +87,7 @@ final class ClerkGuard implements Guard
     {
         $options = new AuthenticateRequestOptions(
             secretKey: config('clerk.secret_key'),
-            authorizedParties: ['http://laravel-clerk.localhost'],
+            authorizedParties: ['http://laravel-clerk.localhost', 'https://laravel-clerk.laravel.cloud'],
             jwtKey: config('clerk.jwt_key'),
         );
 
