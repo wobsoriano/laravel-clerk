5e47b55cbd25435e1d72f8da74242ef2254adfce
wobsoriano <sorianorobertc@gmail.com>
2025-03-02 22:54:08 -0800

    more updates

--- resources/js/pages/settings/profile.tsx
+++ resources/js/pages/settings/profile.tsx
@@ -1,6 +1,6 @@
-import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/react';
-import { UserProfile as ClerkUserProfile } from "@clerk/clerk-react"
+import { SharedData, type BreadcrumbItem } from '@/types';
+import { Head, usePage } from '@inertiajs/react';
+import { UserProfile as ClerkUserProfile, UserProfile } from "@clerk/clerk-react"
 
 import AppLayout from '@/layouts/app-layout';
 import SettingsLayout from '@/layouts/settings/layout';
@@ -12,6 +12,27 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
+const DotIcon = () => {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor">
+        <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z" />
+      </svg>
+    )
+}
+
+
+const CustomPage = () => {
+    const { user } = usePage<{ user: { id: string; email: string } }>().props;
+
+    return (
+      <div>
+        <h1>Custom page</h1>
+        <p>{ JSON.stringify(user) }</p>
+      </div>
+    )
+  }
+  
+
 export default function Profile() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
@@ -19,7 +40,11 @@ export default function Profile() {
 
             <SettingsLayout>
                 <div className="space-y-6">
-                    <ClerkUserProfile />
+                    <ClerkUserProfile>
+                        <UserProfile.Page label="Custom Page" labelIcon={<DotIcon />} url="custom-page">
+                            <CustomPage />
+                        </UserProfile.Page>
+                    </ClerkUserProfile>
                 </div>
 
             </SettingsLayout>
--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,21 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::middleware([])->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 
+    Route::get('/settings/profile', function (ClerkClient $clerkClient) {
+        $user = $clerkClient->getClient()->users->get(Auth::id());
+
+        return Inertia::render('settings/profile', [
+            'user' => $user->user,
+        ]);
+    })->name('settings.profile');
+});
+
+Route::middleware('guest')->group(function () {
     Route::get('/sign-in', function () {
         return Inertia::render('auth/sign-in');
     })->name('sign-in');
@@ -21,20 +31,6 @@ Route::middleware([])->group(function () {
     Route::get('/sign-up', function () {
         return Inertia::render('auth/sign-up');
     })->name('sign-up');
-
-    Route::get('/settings/profile', function () {
-        return Inertia::render('settings/profile');
-    })->name('settings.profile');
-});
-
-Route::get('/api/user', function (ClerkClient $clerkClient) {
-    if (!Auth::check()) {
-        return response()->json(['error' => 'Unauthorized'], 401);
-    }
-
-    $user = $clerkClient->getClient()->users->get(Auth::id());
-
-    return response()->json($user);
 });
 
 // Route::middleware(['auth', 'verified'])->group(function () {
